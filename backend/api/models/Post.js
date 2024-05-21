bdd94a811d7cd8ef1d9468e6022a48eda3310ac6
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-05-21 11:55:00 -0400

    add the post  schema

---
+++ backend/api/models/Post.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const Post = model("Post", postSchema);
+
+export default Post;
