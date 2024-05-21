e06047c0cea35bf34fcc5dd6df23536b6d0a5017
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-05-21 17:19:46 -0400

    create followUser and unfollowUser mutations

--- backend/src/mutations/update.js
+++ backend/src/mutations/update.js
@@ -10,6 +10,36 @@ const updateMutations = {
   updateComment: async (_, { commentId, input }) => {
     return await Comment.findByIdAndUpdate(commentId, input, { new: true });
   },
+  followUser: async (_, { input }) => {
+    const { userId, targetUserId } = input;
+
+    // Add targetUserId to the user's following list
+    const user = await User.findByIdAndUpdate(userId, {
+      $addToSet: { following: targetUserId }
+    }, { new: true }).populate('following');
+
+    // Add userId to the target user's followers list
+    await User.findByIdAndUpdate(targetUserId, {
+      $addToSet: { followers: userId }
+    });
+
+    return user;
+  },
+  unfollowUser: async (_, { input }) => {
+    const { userId, targetUserId } = input;
+
+    // Remove targetUserId from the user's following list
+    const user = await User.findByIdAndUpdate(userId, {
+      $pull: { following: targetUserId }
+    }, { new: true }).populate('following');
+
+    // Remove userId from the target user's followers list
+    await User.findByIdAndUpdate(targetUserId, {
+      $pull: { followers: userId }
+    });
+
+    return user;
+  },
 };
 
 export default updateMutations;
