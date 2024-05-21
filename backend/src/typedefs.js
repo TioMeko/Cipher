a0a4f1088519e146df0e5581eacba3083596c781
Tio Meko <61386052+TioMeko@users.noreply.github.com>
2024-05-21 11:58:10 -0400

    create typeDefs needed for graphQL

---
+++ backend/src/typedefs.js
@@ -0,0 +1,82 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  scalar Date
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    createdAt: Date!
+    posts: [Post]
+    comments: [Comment]
+  }
+
+  type Post {
+    _id: ID!
+    user: User!
+    content: String!
+    createdAt: Date!
+    comments: [Comment]
+  }
+
+  type Comment {
+    _id: ID!
+    user: User!
+    post: Post!
+    content: String!
+    createdAt: Date!
+  }
+
+  input CreateUserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  input UpdateUserInput {
+    username: String
+    email: String
+    password: String
+  }
+
+  input CreatePostInput {
+    userId: ID!
+    content: String!
+  }
+
+  input UpdatePostInput {
+    content: String
+  }
+
+  input CreateCommentInput {
+    userId: ID!
+    postId: ID!
+    content: String!
+  }
+
+  input UpdateCommentInput {
+    content: String
+  }
+
+  type Query {
+    getAllUsers: [User]
+    getUser(userId: ID!): User
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User
+    updateUser(userId: ID!, input: UpdateUserInput!): User
+    deleteUser(userId: ID!): Boolean
+
+    createPost(input: CreatePostInput!): Post
+    updatePost(postId: ID!, input: UpdatePostInput!): Post
+    deletePost(postId: ID!): Boolean
+
+    createComment(input: CreateCommentInput!): Comment
+    updateComment(commentId: ID!, input: UpdateCommentInput!): Comment
+    deleteComment(commentId: ID!): Boolean
+  }
+`;
+
+export default typeDefs;
